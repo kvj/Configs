eb017657bb100e3cff55bd17dfd2f634e7ba0365
Konstantin Vorobyev <vorobyev@coacha.com>
2015-06-17 18:16:40 +0900

    Making config (better agenda, new hotkeys)

--- emacs/org.el
+++ emacs/org.el
@@ -24,6 +24,7 @@
 (setq org-drawers '("PROPERTIES" "CLOCK" "PATH"))
 (setq org-habit-done-word "MADE")
 (setq org-modules (quote (org-docview org-info org-habit org-mew org-mhe org-timer)))
+(defvar k-org-capture-inbox "main.org")
 (setq org-todo-keywords
            '((sequence "TODO(t)" "WAIT(w@/@)" "|" "DONE(d@/@)" "CANCEL(c@)")
              (sequence "|" "APPT(a)" "NOTE(n)" "JOURN(j)")
@@ -49,7 +50,8 @@
 (setq org-agenda-dim-blocked-tasks nil)
 (setq org-agenda-inhibit-startup nil)
 (setq org-agenda-use-tag-inheritance nil)
-(require 'org)
+(setq org-agenda-start-with-log-mode t)
+(require 'org-install)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
@@ -70,18 +72,25 @@
 				(org-agenda-files (list (concat org-directory "main.org")))
 				(org-agenda-sorting-strategy '(todo-state-up priority-down effort-up))
 			))
+			(alltodo "Inbox" (
+				(org-agenda-overriding-header "Inbox")
+				(org-agenda-files (directory-files org-directory t "i_.*.org$"))
+				(org-agenda-sorting-strategy '(todo-state-up priority-down effort-up))
+			))
 		) (
 			(org-agenda-compact-blocks t)
 		)
 	)
 ))
 (setq org-agenda-todo-ignore-scheduled t)
+(defvar k-org-capture-inbox "main.org")
 (setq org-capture-templates
 	'(
 		("t" "Todo" entry (file+headline (concat org-directory "main.org") "Calendar") "* TODO %?")
 		("a" "Appontment" entry (file+headline (concat org-directory "main.org") "Calendar") "* APPT %?")
 		;("b" "Bug" entry (file+headline (concat org-directory "main.org") "Journal") "* BUG %?\n  %u")
 		("n" "Note" entry (file+headline (concat org-directory "main.org") "Journal") "* %T %?")
+		("p" "Todo (inbox)" entry (file+headline (concat org-directory k-org-capture-inbox) "Inbox") "* TODO %?")
 		;("j" "Journal" entry (file+datetree (concat org-directory "journal.org")) "* JOURN %<%H:%M> %?")
 	)
 )
@@ -108,10 +117,37 @@
       (org-display-inline-images)
     (error nil)))
 (setq org-babel-results-keyword "results")
+
 (add-hook 'org-mode-hook
   '(lambda ()
    (setq org-file-apps
      (append '(("\\.png\\'" . default)) org-file-apps ))))
+
+(add-hook 'org-agenda-mode-hook
+  '(lambda ()
+	(org-defkey org-agenda-mode-map "x" 
+				(lambda () 
+				  (interactive) 
+				  (org-agenda-exit)
+				  (save-buffers-kill-terminal)))
+	(org-defkey org-agenda-mode-map "p" 
+				(lambda () 
+				  (interactive) 
+				  (org-capture nil "p")))
+	(org-defkey org-agenda-mode-map "n" 
+				(lambda () 
+				  (interactive) 
+				  (org-capture nil "n")))))
+(add-hook 'org-capture-mode-hook
+  '(lambda ()
+	(org-defkey org-capture-mode-map "\\"
+				(lambda () 
+				  (interactive) 
+				  (org-capture-finalize nil)))
+	(org-defkey org-capture-mode-map "|"
+				(lambda () 
+				  (interactive) 
+				  (org-capture-kill)))))
 (setq org-confirm-babel-evaluate nil)
 (defvar k-org-auto-open-agenda-key nil)
 (add-hook 'emacs-startup-hook
