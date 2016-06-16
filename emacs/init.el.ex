82d6fc421a1839b424170ca413d410cbd64a3967
Konstantin Vorobyev <kvorobyev@gmail.com>
2016-06-16 21:31:11 +0900

    Better start file

--- emacs/init.el.ex
+++ emacs/init.el.ex
@@ -1,5 +1,7 @@
 (setq config-dir "~/Dev/Configs/emacs/")
 (setq org-directory "~/ownCloud/org/")
 
+;(setq k-org-capture-inbox "m_phone.org")
+;(setq k-org-auto-open-agenda-key "w")
+;(setq k-org-goto-zero t)
 (load (concat config-dir "init.el"))
-
