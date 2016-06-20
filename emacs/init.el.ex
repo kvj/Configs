1781788d177ccca624181fe00f68d3b2ae280280
Konstantin Vorobyev <vorobyev@coacha.com>
2016-06-20 14:55:04 +0900

    Calendar config key

--- emacs/init.el.ex
+++ emacs/init.el.ex
@@ -1,7 +1,8 @@
 (setq config-dir "~/Dev/Configs/emacs/")
 (setq org-directory "~/ownCloud/org/")
 
-;(setq k-org-capture-inbox "m_phone.org")
-;(setq k-org-auto-open-agenda-key "w")
-;(setq k-org-goto-zero t)
+;(setq k-org-capture-inbox "m_phone.org") ; Failsafe inbox file
+;(setq k-org-auto-open-agenda-key "w") ; Auto-open agenda on start
+;(setq k-org-goto-zero t) ; Jump to outline first char
+;(setq org-popup-calendar-for-date-prompt nil) ; Do not popup calendar on date input
 (load (concat config-dir "init.el"))
